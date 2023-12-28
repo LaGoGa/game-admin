db584112d4f2c7118c415c1ebf23d89df8e81aa7
Piotr Gorski <mail.piotr.gorski@gmail.com>
2023-12-28 19:00:38 +0100

    [86bwvpxgv] Added: RootPage, AuthPage, router

---
+++ src/router.tsx
@@ -0,0 +1,27 @@
+import { RootRoute, Route, Router } from "@tanstack/react-router";
+import RootPage from "./pages/RootPage";
+import AuthPage from "./pages/AuthPage";
+
+const rootRoute = new RootRoute();
+
+const homeRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: RootPage,
+});
+
+const authRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "authPage",
+  component: AuthPage,
+});
+
+const routeTree = rootRoute.addChildren([homeRoute, authRoute]);
+
+export const router = new Router({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
