8048e52eb94c905db4854f68a616df356069ed98
Piotr Gorski <mail.piotr.gorski@gmail.com>
2023-12-30 00:31:04 +0100

    [86bwvpxgv] Modified: router, RootPage

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -3,7 +3,7 @@ import { Link } from "@tanstack/react-router";
 export default function RootPage() {
   return (
     <section>
-      <Link to="/authPage">Auth Page</Link>
+      <Link to="/authentication">Auth Page</Link>
       <h2>"Hello world"</h2>
     </section>
   );
