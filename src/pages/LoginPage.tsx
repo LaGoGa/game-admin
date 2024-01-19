d8a4e537fa5590e99ac3daf1b9a7fd1679727392
Piotr Gorski <mail.piotr.gorski@gmail.com>
2024-01-19 20:08:16 +0100

    Minor commit for doing it later

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,14 @@
-import { Link } from "@tanstack/react-router";
-
 export default function LoginPage() {
   return (
     <section>
-      <h2>Hello login page</h2>
+      <div className="flex">
+        <div className="md:max-md">
+          <img className="md:max-md" src="./src/assets/_41f5958b-daaa-4131-9fec-8b3900cc29f6.jpg" alt="" />
+        </div>
+        <div className="text-center">
+          <h2 className="">This is my form</h2>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
