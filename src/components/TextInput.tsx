1b6dc65414ffe7645447b1e1bd6fc04d91efcbe3
piotrgorski23 <p.gorski@zdrojowahotels.pl>
2024-01-21 18:38:35 +0100

    [86bwwa8vr] Modified: TextInput style

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -3,11 +3,16 @@ import { useState } from "react";
 export default function TextInput() {
   const [textValue, setTextValue] = useState("");
   return (
-    <input
-      className="rounded border-2 border-solid border-indigo-400"
-      type="text"
-      value={textValue}
-      onChange={event => setTextValue(event.target.value)}
-    />
+    <div className="flex flex-col">
+      <label className="font-bold text-base">Login</label>
+      <span className="font-light text-sm">Write your login below</span>
+      <input
+        className="rounded border-2 border-solid border-indigo-400  "
+        type="text"
+        value={textValue}
+        placeholder="Random shit"
+        onChange={event => setTextValue(event.target.value)}
+      />
+    </div>
   );
 }
