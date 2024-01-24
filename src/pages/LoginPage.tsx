ecf4640f04b39fbf6c3ad184625df1fa7151549d
Piotr Gorski <mail.piotr.gorski@gmail.com>
2024-01-24 16:20:16 +0100

    [86bwwa8vr] Modified: LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,30 +1,12 @@
-import TextInput from "@/components/TextInput";
-import Button from "@/components/Button";
-
+import LoginForm from "@/components/LoginForm";
 export default function LoginPage() {
-  const handleLogin = (value: string) => console.log("Login: ", value);
-  const handlePassword = (value: string) => console.log("Password: ", value);
-
   return (
     <main className="grid min-h-screen grid-cols-1 md:grid-cols-2">
       <div className="hidden md:block">
         <img className="h-full" src="/images/login.jpg" alt="login image" />
       </div>
       <div className="flex flex-col items-center justify-center">
-        <div>
-          <TextInput onChange={handleLogin} label="Login" description="Write your login below" placeholder="Login" />
-        </div>
-        <div>
-          <TextInput
-            onChange={handlePassword}
-            label="Password"
-            description="Write your password below"
-            placeholder="Password"
-          />
-        </div>
-        <div>
-          <Button />
-        </div>
+        <LoginForm />
       </div>
     </main>
   );
